f2b82ec61ab987754737334a045f7067f262d66f
Benjamin <Heng>
2013-01-10 01:36:16 +0800

    Now generates a html file for viewing drops. (forgot to commit these)

--- README.txt
+++ README.txt
@@ -1,5 +1,5 @@
 [INFORMATION]
-TF2DropMonitor monitors the inventories of specified accounts for new items. The new items are then displayed to the user. The primary purpose of TF2DropMonitor is to allow you to easily view what your idle accounts have found.
+TF2DropMonitor monitors the inventories of specified accounts for new items. The new items are then written to a html file for easy viewing. The primary purpose of TF2DropMonitor is to allow you to easily view what your idle accounts have found.
 
 It is written in python and the source code is available for you to freely edit, adapt and redistribute. The python library 'steamodd' is used to interact with Steam.
 
@@ -8,14 +8,23 @@ It is written in python and the source code is available for you to freely edit,
 [CONFIGURATION]
 Configuration file: TF2DropMonitor.ini
 
-'accounts': A list of steamIDs to monitor, separated by commas. The steamIDs must correspond to the custom URL of the account. For instance, if my profile is 'http://steamcommunity.com/id/wryyl', then my steamID is 'wryyl'. The custom URL can be set by editing your steam profile.
+accounts: A list of steamIDs to monitor, separated by commas. The steamIDs must correspond to the custom URL of the account. For instance, if my profile is 'http://steamcommunity.com/id/wryyl', then my steamID is 'wryyl'. The custom URL can be set by editing your steam profile.
     Example: account1,account2,account3
 
-'api_key': Your API key. You can get your key at 'http://steamcommunity.com/dev/apikey'. Just enter 'google.com' or something as your domain name. This is needed to retrieve backpack information.
+api_key: Your API key. You can get your key at 'http://steamcommunity.com/dev/apikey'. Just enter 'google.com' or something as your domain name. This is needed to retrieve backpack information.
     Example: B8BF6FDG536JLK2AB1BD8184E88MB2C9
 
-'poll_minutes': The number of minutes to wait before each inventory check.
+poll_minutes: The number of minutes to wait before each inventory check.
     Recommended: 3
 
-'logging': Whether to log your drops to a file or not.
+logging: Whether to log your drops to a file or not.
     '1' to enable, '0' to disable.
+
+html_dir: Path to where you want to generate the html in. The html will be generated in index.html in this directory.
+
+
+------------------------------------------------------------------
+[NOTES]
+In the directory specified by the html_dir variable, ensure that you have the base index.html and style.css inside.
+
+You can set the script to write index.html to your webserver, thereby allowing you to view your item drops online.
--- TF2DropMonitor.py
+++ TF2DropMonitor.py
@@ -1,10 +1,12 @@
+#!/usr/bin/python
+
 import steamodd as steam
 import thread
 import threading
 import time
 import string
 import ConfigParser
-import os
+import os, sys
 
 class DropThread(threading.Thread):
     def __init__(self, steamId):
@@ -28,7 +30,8 @@ class DropThread(threading.Thread):
                     itemname = i.get_name()
                     item.name = itemname
                     item.id = str(i.get_id())
-                    item.time = time.strftime('%d/%m/%y %H:%M', time.localtime(time.time()))
+                    #item.time = time.strftime('%d/%m/%y %H:%M', time.localtime(time.time()))
+                    item.time = time.strftime('%d/%m/%y %H:%M', time.localtime(time.time()+28800))
                     material = i.get_craft_material_type()
                     
                     if material != None:
@@ -88,19 +91,15 @@ class DropThread(threading.Thread):
         else:
             newestItems = []
             for item in backpack:
-                if item.get_id() > self.lastId:
+                if item.get_id() > self.lastId and item.get_id() == item.get_original_id():
+                #if item.get_id() > self.lastId:
                     newestItems.append(item)
             if len(newestItems) != 0:
                 self.lastId = max([item.get_id() for item in newestItems])
             return newestItems
             
     def dropEvent(self, item):
-        msg = '%s  | %s | %s | %s' %(item.time,
-                                string.ljust(item.account, 12),
-                                string.ljust(item.material, 6),
-                                item.name)
-        print msg
-        log.log(msg)
+        log.log(item)
 
 class Item:
     def __init__(self):
@@ -117,29 +116,58 @@ class Options:
         self.apiKey = ''
         self.pollMinutes = 1
         self.logging = 0
+        self.htmldir = ''
         self.get_config_options()
 
     def get_config_options(self):
         try:
-            self.conf.read(os.path.join(os.getcwd(), 'TF2DropMonitor.ini'))
+            self.conf.read(os.path.join(os.path.dirname(sys.argv[0]), 'TF2DropMonitor.ini'))
             self.accounts = self.conf.get('General', 'accounts').split(',')
             self.apiKey = self.conf.get('General', 'api_key')
             self.pollMinutes = int(self.conf.get('General', 'poll_minutes'))
             self.logging = int(self.conf.get('General', 'logging'))
+            self.htmldir = self.conf.get('General', 'html_dir')
         except Exception as e:
             print 'Options Error:', e
             return
 
 class Log:
     def __init__(self):
-        self.logfile = os.path.join(os.getcwd(), 'TF2DropMonitor.log')
+        self.logfile = os.path.join(os.path.dirname(sys.argv[0]), 'TF2DropMonitor.log')
         self.enabled = 0
 
-    def log(self, msg):
+
+    def log(self, item):
         if self.enabled:
+            msg = '%s  | %s | %s | %s' %(item.time,
+                                string.ljust(item.account, 12),
+                                string.ljust(item.material, 6),
+                                item.name)
+            print msg
+            self.log_html(item)
             with open(self.logfile, 'a') as f:
                 f.write(msg + '\r\n')
 
+    def log_html(self, item):
+        row = self.construct_row(item)
+        row = '%s\n<!--APPENDROWHERE-->' % row
+        htmlfilepath = os.path.join(options.htmldir, 'index.html')
+        try:
+            with open(htmlfilepath, 'r') as f:
+                content = f.read()
+                newContent = content.replace('<!--APPENDROWHERE-->', row)
+            with open(htmlfilepath, 'w') as f:
+                f.write(newContent)
+        except:
+            pass
+
+    def construct_row(self, item):
+        template = '<tr><td class="noborder"></td><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>'
+        templateHat = '<tr class="hat-highlight"><td class="noborder">></td><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>'
+        if item.material.lower() in ['hat', 'misc']:
+            template = templateHat
+        row = template % (item.time, item.account, item.material.capitalize(), item.name)
+        return row
 
 if __name__ == '__main__':
     options = Options()
